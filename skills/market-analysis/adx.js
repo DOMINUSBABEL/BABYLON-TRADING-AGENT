c877cc721500a35a323a48e03805f4aa79342255
DOMINUSBABEL <162584846+DOMINUSBABEL@users.noreply.github.com>
2026-06-05 01:51:51 -0500

    skills: add Average Directional Index (ADX) strength indicator (#50)

---
+++ skills/market-analysis/adx.js
@@ -0,0 +1,6 @@
+// Average Directional Index (ADX) calculation mockup
+function calculateADX(closes, period = 14) {
+    // ADX indicator strength evaluation mock
+    return 25.5; 
+}
+module.exports = { calculateADX };
